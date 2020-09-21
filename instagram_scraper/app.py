af6f6dcb08c94b2b125c1308c08b1b7336f31790
Louis Gruand <louisgruand@gmail.com>
2020-09-21 23:34:44 +0200

    Update app.py

--- instagram_scraper/app.py
+++ instagram_scraper/app.py
@@ -18,7 +18,7 @@ import sys
 import textwrap
 import time
 import xml.etree.ElementTree as ET
-import moviepy.editor as mpe
+# import moviepy.editor as mpe
 
 try:
     from urllib.parse import urlparse
@@ -541,6 +541,7 @@ class InstagramScraper(object):
                 self.logger.exception('Failed to query ' + query)
 
     def __query(self, url, variables, entity_name, query, end_cursor):
+        # resp is not the right one for a single image: date is wrong, no accessibility caption, no tagged_users
         params = variables.format(query, end_cursor)
         self.update_ig_gis_header(params)
 
@@ -557,18 +558,21 @@ class InstagramScraper(object):
                     nodes.extend(self._get_nodes(top_posts))
 
                 posts = payload['edge_' + entity_name + '_to_media']
-
                 nodes.extend(self._get_nodes(posts))
+
                 end_cursor = posts['page_info']['end_cursor']
                 return nodes, end_cursor
 
         return None, None
 
     def _get_nodes(self, container):
+        # PB is: node does not contain users!
         return [self.augment_node(node['node']) for node in container['edges']]
 
     def augment_node(self, node):
+        # PB is: node does not contain users!
         self.extract_tags(node)
+        self.extract_tagged_users(node)
 
         details = None
         if self.include_location and 'location' not in node:
@@ -577,28 +581,40 @@ class InstagramScraper(object):
 
         if 'urls' not in node:
             node['urls'] = []
+            node['tagged_user'] = self.extract_tagged_users(node)
         if node['is_video'] and 'video_url' in node:
             node['urls'] = [node['video_url']]
+            node['tagged_user'] = self.extract_tagged_users(node)
         elif '__typename' in node and node['__typename'] == 'GraphImage':
             node['urls'] = [self.get_original_image(node['display_url'])]
+            node['tagged_user'] = self.extract_tagged_users(node)
+
+
         else:
             if details is None:
                 details = self.__get_media_details(node['shortcode'])
 
             if details:
+
                 if '__typename' in details and details['__typename'] == 'GraphVideo':
-                    node['urls'] = [details['video_url']]
+                    node['urls'] = [details['video_url']]      
+                    node['tagged_user'] = self.extract_tagged_users(node)          
+                    
                 elif '__typename' in details and details['__typename'] == 'GraphSidecar':
                     urls = []
                     for carousel_item in details['edge_sidecar_to_children']['edges']:
                         urls += self.augment_node(carousel_item['node'])['urls']
+                    # new addition: get all the tagged users in carousel
                     node['urls'] = urls
+                    node['tagged_user'] = self.extract_tagged_users(details)
                 else:
                     node['urls'] = [self.get_original_image(details['display_url'])]
-
+                    node['tagged_user'] = self.extract_tagged_users(details)
+        
         return node
 
     def __get_media_details(self, shortcode):
+        """Goes into here only when GraphSidecar aka when the post is a collection of media."""
         resp = self.get_json(VIEW_MEDIA_URL.format(shortcode))
 
         if resp is not None:
@@ -972,15 +988,19 @@ class InstagramScraper(object):
                 self.logger.exception('Failed to query media for user ' + user['username'])
 
     def __query_media(self, id, end_cursor=''):
+        # resp does not contain tagged user
         params = QUERY_MEDIA_VARS.format(id, end_cursor)
         self.update_ig_gis_header(params)
+
 
         resp = self.get_json(QUERY_MEDIA.format(params))
 
         if resp is not None:
+            # resp does not contain tagged_user
             payload = json.loads(resp)['data']['user']
 
             if payload:
+                # payload does not contain tagged_user
                 container = payload['edge_owner_to_timeline_media']
                 nodes = self._get_nodes(container)
                 end_cursor = container['page_info']['end_cursor']
@@ -1026,6 +1046,26 @@ class InstagramScraper(object):
             return True
 
         return False
+    
+    def extract_tagged_users(self, item):
+        """Extracts the tagged users from a post."""
+        
+        # for single images the item does not even appear here
+
+
+        tagged_user_list = []
+        item['tagged_user'] = '0'
+
+        if 'edge_media_to_tagged_user' in item and item['edge_media_to_tagged_user'] and item['edge_media_to_tagged_user'][
+            'edges'] and item['edge_media_to_tagged_user']['edges'][0]["node"]:
+            for tagged_user in item['edge_media_to_tagged_user']['edges']:
+                tagged_user_list.append(tagged_user['node']['user']['username'])
+            item['tagged_user'] = list(set(tagged_user_list))
+
+
+        return item['tagged_user']
+
+
 
     def extract_tags(self, item):
         """Extracts the hashtags from the caption text."""
@@ -1046,7 +1086,7 @@ class InstagramScraper(object):
                 r"(?<!&)#(\w+|(?:[\xA9\xAE\u203C\u2049\u2122\u2139\u2194-\u2199\u21A9\u21AA\u231A\u231B\u2328\u2388\u23CF\u23E9-\u23F3\u23F8-\u23FA\u24C2\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE\u2600-\u2604\u260E\u2611\u2614\u2615\u2618\u261D\u2620\u2622\u2623\u2626\u262A\u262E\u262F\u2638-\u263A\u2648-\u2653\u2660\u2663\u2665\u2666\u2668\u267B\u267F\u2692-\u2694\u2696\u2697\u2699\u269B\u269C\u26A0\u26A1\u26AA\u26AB\u26B0\u26B1\u26BD\u26BE\u26C4\u26C5\u26C8\u26CE\u26CF\u26D1\u26D3\u26D4\u26E9\u26EA\u26F0-\u26F5\u26F7-\u26FA\u26FD\u2702\u2705\u2708-\u270D\u270F\u2712\u2714\u2716\u271D\u2721\u2728\u2733\u2734\u2744\u2747\u274C\u274E\u2753-\u2755\u2757\u2763\u2764\u2795-\u2797\u27A1\u27B0\u27BF\u2934\u2935\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55\u3030\u303D\u3297\u3299]|\uD83C[\uDC04\uDCCF\uDD70\uDD71\uDD7E\uDD7F\uDD8E\uDD91-\uDD9A\uDE01\uDE02\uDE1A\uDE2F\uDE32-\uDE3A\uDE50\uDE51\uDF00-\uDF21\uDF24-\uDF93\uDF96\uDF97\uDF99-\uDF9B\uDF9E-\uDFF0\uDFF3-\uDFF5\uDFF7-\uDFFF]|\uD83D[\uDC00-\uDCFD\uDCFF-\uDD3D\uDD49-\uDD4E\uDD50-\uDD67\uDD6F\uDD70\uDD73-\uDD79\uDD87\uDD8A-\uDD8D\uDD90\uDD95\uDD96\uDDA5\uDDA8\uDDB1\uDDB2\uDDBC\uDDC2-\uDDC4\uDDD1-\uDDD3\uDDDC-\uDDDE\uDDE1\uDDE3\uDDEF\uDDF3\uDDFA-\uDE4F\uDE80-\uDEC5\uDECB-\uDED0\uDEE0-\uDEE5\uDEE9\uDEEB\uDEEC\uDEF0\uDEF3]|\uD83E[\uDD10-\uDD18\uDD80-\uDD84\uDDC0]|(?:0\u20E3|1\u20E3|2\u20E3|3\u20E3|4\u20E3|5\u20E3|6\u20E3|7\u20E3|8\u20E3|9\u20E3|#\u20E3|\\*\u20E3|\uD83C(?:\uDDE6\uD83C(?:\uDDEB|\uDDFD|\uDDF1|\uDDF8|\uDDE9|\uDDF4|\uDDEE|\uDDF6|\uDDEC|\uDDF7|\uDDF2|\uDDFC|\uDDE8|\uDDFA|\uDDF9|\uDDFF|\uDDEA)|\uDDE7\uD83C(?:\uDDF8|\uDDED|\uDDE9|\uDDE7|\uDDFE|\uDDEA|\uDDFF|\uDDEF|\uDDF2|\uDDF9|\uDDF4|\uDDE6|\uDDFC|\uDDFB|\uDDF7|\uDDF3|\uDDEC|\uDDEB|\uDDEE|\uDDF6|\uDDF1)|\uDDE8\uD83C(?:\uDDF2|\uDDE6|\uDDFB|\uDDEB|\uDDF1|\uDDF3|\uDDFD|\uDDF5|\uDDE8|\uDDF4|\uDDEC|\uDDE9|\uDDF0|\uDDF7|\uDDEE|\uDDFA|\uDDFC|\uDDFE|\uDDFF|\uDDED)|\uDDE9\uD83C(?:\uDDFF|\uDDF0|\uDDEC|\uDDEF|\uDDF2|\uDDF4|\uDDEA)|\uDDEA\uD83C(?:\uDDE6|\uDDE8|\uDDEC|\uDDF7|\uDDEA|\uDDF9|\uDDFA|\uDDF8|\uDDED)|\uDDEB\uD83C(?:\uDDF0|\uDDF4|\uDDEF|\uDDEE|\uDDF7|\uDDF2)|\uDDEC\uD83C(?:\uDDF6|\uDDEB|\uDDE6|\uDDF2|\uDDEA|\uDDED|\uDDEE|\uDDF7|\uDDF1|\uDDE9|\uDDF5|\uDDFA|\uDDF9|\uDDEC|\uDDF3|\uDDFC|\uDDFE|\uDDF8|\uDDE7)|\uDDED\uD83C(?:\uDDF7|\uDDF9|\uDDF2|\uDDF3|\uDDF0|\uDDFA)|\uDDEE\uD83C(?:\uDDF4|\uDDE8|\uDDF8|\uDDF3|\uDDE9|\uDDF7|\uDDF6|\uDDEA|\uDDF2|\uDDF1|\uDDF9)|\uDDEF\uD83C(?:\uDDF2|\uDDF5|\uDDEA|\uDDF4)|\uDDF0\uD83C(?:\uDDED|\uDDFE|\uDDF2|\uDDFF|\uDDEA|\uDDEE|\uDDFC|\uDDEC|\uDDF5|\uDDF7|\uDDF3)|\uDDF1\uD83C(?:\uDDE6|\uDDFB|\uDDE7|\uDDF8|\uDDF7|\uDDFE|\uDDEE|\uDDF9|\uDDFA|\uDDF0|\uDDE8)|\uDDF2\uD83C(?:\uDDF4|\uDDF0|\uDDEC|\uDDFC|\uDDFE|\uDDFB|\uDDF1|\uDDF9|\uDDED|\uDDF6|\uDDF7|\uDDFA|\uDDFD|\uDDE9|\uDDE8|\uDDF3|\uDDEA|\uDDF8|\uDDE6|\uDDFF|\uDDF2|\uDDF5|\uDDEB)|\uDDF3\uD83C(?:\uDDE6|\uDDF7|\uDDF5|\uDDF1|\uDDE8|\uDDFF|\uDDEE|\uDDEA|\uDDEC|\uDDFA|\uDDEB|\uDDF4)|\uDDF4\uD83C\uDDF2|\uDDF5\uD83C(?:\uDDEB|\uDDF0|\uDDFC|\uDDF8|\uDDE6|\uDDEC|\uDDFE|\uDDEA|\uDDED|\uDDF3|\uDDF1|\uDDF9|\uDDF7|\uDDF2)|\uDDF6\uD83C\uDDE6|\uDDF7\uD83C(?:\uDDEA|\uDDF4|\uDDFA|\uDDFC|\uDDF8)|\uDDF8\uD83C(?:\uDDFB|\uDDF2|\uDDF9|\uDDE6|\uDDF3|\uDDE8|\uDDF1|\uDDEC|\uDDFD|\uDDF0|\uDDEE|\uDDE7|\uDDF4|\uDDF8|\uDDED|\uDDE9|\uDDF7|\uDDEF|\uDDFF|\uDDEA|\uDDFE)|\uDDF9\uD83C(?:\uDDE9|\uDDEB|\uDDFC|\uDDEF|\uDDFF|\uDDED|\uDDF1|\uDDEC|\uDDF0|\uDDF4|\uDDF9|\uDDE6|\uDDF3|\uDDF7|\uDDF2|\uDDE8|\uDDFB)|\uDDFA\uD83C(?:\uDDEC|\uDDE6|\uDDF8|\uDDFE|\uDDF2|\uDDFF)|\uDDFB\uD83C(?:\uDDEC|\uDDE8|\uDDEE|\uDDFA|\uDDE6|\uDDEA|\uDDF3)|\uDDFC\uD83C(?:\uDDF8|\uDDEB)|\uDDFD\uD83C\uDDF0|\uDDFE\uD83C(?:\uDDF9|\uDDEA)|\uDDFF\uD83C(?:\uDDE6|\uDDF2|\uDDFC))))[\ufe00-\ufe0f\u200d]?)+",
                 caption_text, re.UNICODE)
             item['tags'] = list(set(item['tags']))
-
+        
         return item
 
     def get_original_image(self, url):
